f12cd2ffaffc37bba31cb6dc2c3121f98bd4f8cd
danblackhurst <daniel@hubpay.ae>
2023-10-16 16:21:31 +0100

    initial test commit

--- src/main.js
+++ src/main.js
@@ -1,27 +1,34 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const { Client } = require('@notionhq/client')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
+  await updateNotionDatabase()
+}
+
+async function updateNotionDatabase() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const notion = new Client({
+      auth: core.getInput('notion-secret', { required: true })
+    })
+    const releaseNotes = process.env.GITHUB_EVENT.release.body
+    const version = process.env.GITHUB_EVENT.release.tag_name
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Update Notion database
+    const response = await notion.databases.update({
+      database_id: process.env.NOTION_DATABASE_ID,
+      properties: {
+        'Release Notes': {
+          rich_text: [{ text: { content: releaseNotes } }]
+        },
+        Version: {
+          rich_text: [{ text: { content: version } }]
+        }
+      }
+    })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Notion database updated successfully:${response}`)
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    core.error('Error updating Notion database:', error)
   }
 }
 
